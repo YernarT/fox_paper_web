f4ee718fa6fa1e67468ac9e77ea9ed3ed39d837d
YernarT <toktaryernar@gmail.com>
2023-08-27 01:37:19 +0600

    feat: :sparkles: 表格背景

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -14,12 +14,12 @@ import Footer from "~/components/default_layout/Footer/index.vue";
 <style scoped>
 .default-layout {
   position: relative;
-  background: var(--c-background);
-
-  @media screen and (max-width: 600px) {
-    .navbar {
-      display: none;
-    }
-  }
+  background: linear-gradient(rgba(0, 0, 0, 0.25) 1px, transparent 0px) 0% 0% /
+      15px 15px,
+    linear-gradient(90deg, rgba(0, 0, 0, 0.25) 1px, transparent 0px) 0% 0% /
+      15px 15px,
+    linear-gradient(rgb(255, 255, 255) 1px, transparent 0px) 0% 0% / 75px 75px,
+    linear-gradient(90deg, rgb(255, 255, 255) 1px, transparent 0px) 0% 0% / 75px
+      75px rgb(255, 255, 255);
 }
 </style>
