0ecf8bd2522bee7f5b5028e2d9ee146bbb0d779b
YernarT <toktaryernar@gmail.com>
2023-08-27 01:36:41 +0600

    feat: :sparkles: simple button

---
+++ src/components/common/Button/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+    class="btn"
+    :class="{ [`btn--${props.variant ?? 'outlined'}`]: true }"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+// Default variant is `outlined`
+type T_ButtonVariants = "primary" | "outlined" | "ghot";
+
+interface I_ButtonProps {
+  variant?: T_ButtonVariants;
+}
+
+const props = defineProps<I_ButtonProps>();
+</script>
+
+<style scope lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.btn {
+  outline: none;
+  border: none;
+  padding: 8px 14px;
+  border: 2px solid var(--c-primary);
+  background: transparent;
+  cursor: pointer;
+  transition: transform var(--transition);
+
+  &--primary {
+    color: #fff;
+    background: var(--c-primary);
+  }
+
+  &--ghot {
+    border-color: transparent;
+    background: transparent;
+  }
+
+  &:active {
+    transform: scale(0.9);
+  }
+}
+</style>
