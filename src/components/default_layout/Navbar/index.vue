c201c85461f4a714daf1a1f9d8c8a027ab7e46d6
YernarT <toktaryernar@gmail.com>
2023-08-27 01:37:06 +0600

    init: :rainbow: Navbar

--- src/components/default_layout/Navbar/index.vue
+++ src/components/default_layout/Navbar/index.vue
@@ -3,37 +3,20 @@
     <Logotyp class="logotyp" />
 
     <nav class="block">
-      <NuxtLink
-        v-for="link in navLinks"
-        :key="link.path"
-        :to="link.path"
-        class="nav-link"
-        :class="{ 'nav-link--active': linkIsActive(link.path) }"
-      >
-        {{ link.label }}
-      </NuxtLink>
+      <Button variant="primary" @click="modal.authModal.open = true">
+        <span>Опубликовать обсуждение</span>
+      </Button>
+      <Button v-if="!user.isAuthenticated" @click="modal.authModal.open = true">
+        <span>Бесплатная регистрация</span>
+      </Button>
 
-      <a-button
-        v-if="!user.isAuthenticated"
-        type="primary"
-        class="action-btn"
-        @click="modal.authModal.open = true"
-      >
-        <span>Регистрация</span>
-      </a-button>
-
-      <a-button
-        v-if="user.isAuthenticated"
-        danger
-        class="action-btn"
-        @click="user.logout"
-      >
+      <Button v-if="user.isAuthenticated" @click="user.logout">
         <span>Выйти</span>
-      </a-button>
+      </Button>
 
-      <ClientOnly>
+      <!-- <ClientOnly>
         <AuthModal />
-      </ClientOnly>
+      </ClientOnly> -->
     </nav>
   </header>
 </template>
@@ -47,25 +30,7 @@ import { useGlobalModal } from "~/stores/modal";
 // Component
 import Logotyp from "../Logotyp.vue";
 import AuthModal from "../AuthModal/index.vue";
-
-const navLinks = [
-  {
-    path: "/",
-    label: "Глвный",
-  },
-  {
-    path: "/products",
-    label: "Продукты",
-  },
-  {
-    path: "/contact-us",
-    label: "Контакт",
-  },
-  {
-    path: "/faq",
-    label: "FAQ",
-  },
-];
+import Button from "~/components/common/Button/index.vue";
 
 const user = useUser();
 const route = useRoute();
@@ -79,83 +44,14 @@ const linkIsActive = computed(() => (path: string) => path === route.path);
 
 .navbar {
   height: 75px;
+  border-bottom: 1px solid var(--c-border);
+  background: var(--c-background);
   user-select: none;
   @include flex($alignItems: center);
 
   .block {
     margin-left: auto;
-    @include flex($alignItems: center, $gap: 60px);
-  }
-
-  .nav-link {
-    position: relative;
-    @include flexCenter;
-
-    color: var(--c-text);
-    font-weight: 700;
-    text-decoration: none;
-
-    outline: none;
-    cursor: pointer;
-    transition: color var(--transition);
-
-    &::after {
-      content: "";
-      width: 100%;
-      height: 3px;
-      border-radius: var(--border-radius);
-      background: var(--c-primary);
-      @include positioned($top: calc(100% + 3px));
-
-      transform: scaleX(0);
-      transform-origin: top left;
-      transition: transform var(--transition);
-    }
-
-    &--active {
-      color: var(--c-primary);
-    }
-
-    &--active,
-    &:hover,
-    &:focus {
-      &::after {
-        transform: scaleX(1);
-      }
-    }
-  }
-
-  .action-btn {
-    @include flexCenter;
-
-    span {
-      font-weight: 700;
-    }
-  }
-
-  @media screen and (max-width: 1290px) {
-    .block {
-      gap: 40px;
-    }
-  }
-
-  @media screen and (max-width: 1080px) {
-    height: 144px;
-    padding: 24px 0;
-    flex-direction: column;
-    gap: 24px;
-
-    .block {
-      margin: 0;
-      justify-content: center;
-      gap: 60px;
-    }
-  }
-
-  @media screen and (max-width: 768px) {
-    .block {
-      gap: 40px;
-    }
+    @include flex($alignItems: center, $gap: 24px);
   }
 }
 </style>
