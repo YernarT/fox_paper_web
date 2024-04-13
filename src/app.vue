8c6db3ce0f740d435a0d695a9d96c30aecca083d
YernarT <toktaryernar@gmail.com>
2024-04-13 20:19:19 +0500

    refactor: :recycle: reborn project

--- src/app.vue
+++ src/app.vue
@@ -3,20 +3,20 @@
     <Head>
       <Meta charset="UTF-8" />
       <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <Meta name="theme-color" content="#0093FF" />
+      <Meta name="theme-color" content="#1191ff" />
       <meta name="color-scheme" content="light" />
 
       <Meta httpEquiv="x-ua-compatible" content="IE=edge" />
       <Meta httpEquiv="x-ua-compatible" content="IE=7" />
 
-      <Meta name="description" content="ITReview" />
-      <Meta name="keywords" content="ITReview" />
+      <Meta name="description" content="Fox paper" />
+      <Meta name="keywords" content="Fox paper" />
       <Meta name="author" content="https://github.com/YernarT" />
 
-      <Meta property="og:title" content="IIReview" />
-      <Meta property="og:description" content="ITReview" />
+      <Meta property="og:title" content="Fox Paper" />
+      <Meta property="og:description" content="Fox paper." />
       <Meta property="og:image" content="/favicon.ico" />
-      <Meta property="og:site_name" content="ITReview" />
+      <Meta property="og:site_name" content="Fox paper" />
       <Meta property="og:type" content="website" />
       <Meta property="og:locale" content="ru_RU" />
 
@@ -25,16 +25,18 @@
       <Link rel="preconnect" href="https://fonts.googleapis.com" />
       <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
       <Link
-        href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
         rel="stylesheet"
       />
 
-      <Title>ITReview</Title>
+      <Title>Fox paper</Title>
     </Head>
 
     <Body>
       <NuxtLayout>
-        <NuxtPage />
+        <ConfigProvider :locale="ru_RU" :theme="ANTD_THEME">
+          <NuxtPage />
+        </ConfigProvider>
       </NuxtLayout>
     </Body>
   </Html>
@@ -44,10 +46,30 @@
 // Vue
 import { onBeforeMount } from "vue";
 // Store
-import { useUser } from "~/stores/user";
+import { useUser } from "@/stores/user";
+// Antd
+import ru_RU from "ant-design-vue/locale/ru_RU";
+import { ConfigProvider } from "ant-design-vue";
+import { ANTD_THEME } from "@/constants/antd";
 
+defineOptions({ name: "App" });
+
+// 初始化 Store
 onBeforeMount(() => {
   const user = useUser();
   user.initUserFromLocal();
 });
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity var(--transition), filter var(--transition);
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(2px);
+}
+</style>
