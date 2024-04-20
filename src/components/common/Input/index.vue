cc0862b7573121d884ff3635d4d8c902baa09e3f
YernarT <toktaryernar@gmail.com>
2024-04-21 00:12:27 +0500

    update: :hammer: emit blur, focus, style

--- src/components/common/Input/index.vue
+++ src/components/common/Input/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="input-wrap">
     <Icon v-if="prefixIcon" :name="prefixIcon" />
-    <input v-model="model" :type="type" :placeholder="placeholder" />
+    <input
+      v-model="model"
+      :type="type"
+      :placeholder="placeholder"
+      @blur="emit('blur')"
+      @focus="emit('focus')"
+    />
     <Icon v-if="affixIcon" :name="affixIcon" />
   </div>
 </template>
@@ -13,6 +19,11 @@ import { toRefs } from "vue";
 defineOptions({ name: "Input" });
 
 const model = defineModel<string>("value");
+
+const emit = defineEmits<{
+  (event: "blur"): void;
+  (event: "focus"): void;
+}>();
 
 const props = defineProps({
   prefixIcon: {
@@ -40,11 +51,14 @@ const { prefixIcon, affixIcon, type } = toRefs(props);
 @import "~/assets/style/mixins.scss";
 
 .input-wrap {
+  height: 32px;
   padding: 2px;
   border-bottom: 1px solid var(--c-border);
   @include flex($alignItems: center, $gap: 4px);
 
   input {
+    width: 100%;
+    height: 100%;
     outline: none;
     border: none;
   }
