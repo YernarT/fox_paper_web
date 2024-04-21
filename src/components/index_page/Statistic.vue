6f74bb811fadeb6707b4ffd9254e81749cae3b74
YernarT <toktaryernar@gmail.com>
2024-04-21 12:46:38 +0500

    inti: :rainbow: statistic

---
+++ src/components/index_page/Statistic.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="statistic">
+    <p>Всего 883,343,882 документа</p>
+    <p>Количество пользователей 143,117</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: "Statistic" });
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.statistic {
+  font-size: 14px;
+  color: var(--c-text-muted);
+  @include flex($alignItems: center, $gap: 8px);
+}
+</style>
