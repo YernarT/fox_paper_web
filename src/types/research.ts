03ce5a1a2d681c72eabbd06f800ffc4dda62aa53
YernarT <toktaryernar@gmail.com>
2024-04-23 00:19:38 +0500

    update: :hammer: I_PostResearch

--- src/types/research.ts
+++ src/types/research.ts
@@ -1,4 +1,5 @@
 import type { I_User } from "~/types/user";
+import type { UploadFile } from "ant-design-vue";
 
 export interface I_Category {
   id: number;
@@ -16,5 +17,12 @@ export interface I_Research {
   updatedAt: string;
   publishedAt: string;
   author: I_User;
+  categories: I_Category[];
+}
+
+export interface I_PostResearch {
+  title: string;
+  description: string;
+  file: UploadFile[];
   categories: I_Category[];
 }
