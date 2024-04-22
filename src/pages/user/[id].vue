e320f92956d250a5a2a39968b2359807895781b5
YernarT <toktaryernar@gmail.com>
2024-04-22 10:31:23 +0500

    init: :rainbow: user page

---
+++ src/pages/user/[id].vue
@@ -0,0 +1,55 @@
+<template>
+  <main class="itisit-container user-page">
+    <Tabs v-model:activeKey="activeTab" tab-position="left">
+      <TabPane key="profile">
+        <template #tab>
+          <div class="panel-title">
+            <Icon name="material-symbols:person-edit-sharp" />
+            Профиль
+          </div>
+        </template>
+
+        <UserForm :initValue="toRaw(userStore.$state)" />
+      </TabPane>
+      <TabPane key="research">
+        <template #tab>
+          <div class="panel-title">
+            <Icon name="game-icons:archive-research" />
+            Статья
+          </div>
+        </template>
+
+        Research
+      </TabPane>
+    </Tabs>
+  </main>
+</template>
+
+<script setup lang="ts">
+// Vue
+import { ref } from "vue";
+// Store
+import { useUserStore } from "@/stores/user";
+// Components
+import { Tabs, TabPane } from "ant-design-vue";
+import UserForm from "@/components/user/UserForm.vue";
+
+defineOptions({ name: "UserPage" });
+
+const userStore = useUserStore();
+const activeTab = ref<"profile" | "research">("profile");
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.user-page {
+  min-height: calc(50dvh);
+  padding-top: 40px;
+  padding-bottom: 40px;
+
+  .ant-tabs-tab .panel-title {
+    @include flex($alignItems: center, $gap: 8px);
+  }
+}
+</style>
